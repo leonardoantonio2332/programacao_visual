ce4198f1ad68c90eba67637cc60ea4bd89051a5f
leonardo <leonardoantonio2332@gmail.com>
2021-04-27 23:02:39 -0300

    implementações da atividade5

--- src/MedicoDAO.java
+++ src/MedicoDAO.java
@@ -3,12 +3,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicoDAO {
-	private String DRIVER = "org.postgresql.Driver";
-	private String urlBanco = "jdbc:postgresql://motty.db.elephantsql.com:5432/jiknpecj"; //indica o caminho do banco de dados
-	private String usuarioBanco = "jiknpecj"; // aqui vai o nome usuario que vc quer acessar
-	private String senhaBanco = "WrUJLat69yzU9Kuq-lYhWL1oJ_G_cGxT"; // aqui a senha do seu banco
+//	private String DRIVER = "org.postgresql.Driver";
+	private String urlBanco = "jdbc:postgresql://motty.db.elephantsql.com:5432/jiknpecj"; 
+	private String usuarioBanco = "jiknpecj"; 
+	private String senhaBanco = "WrUJLat69yzU9Kuq-lYhWL1oJ_G_cGxT"; 
 	private Connection getConnection() {
 		Connection connection = null;
 		try
@@ -80,5 +82,82 @@ public class MedicoDAO {
 			e.printStackTrace();
 		}
 		return medico;
+	}
+
+	protected List<Medico> selecionarTodosMedicos() {
+		List<Medico> medicos = new ArrayList<>();
+		try
+		{
+			Connection conexao = getConnection();
+			if (conexao != null)
+			{
+				PreparedStatement consultarMedicos = conexao.prepareStatement("SELECT * FROM medico");
+				ResultSet rs = consultarMedicos.executeQuery();
+				while (rs.next()) 
+				{
+					String nome = rs.getString("nome");
+					String cpf = rs.getString("crm");
+					String especialidade = rs.getString("especialidade");
+					medicos.add(new Medico(nome,cpf,especialidade));
+				}	
+				rs.close();
+				consultarMedicos.close();
+				conexao.close();
+			}
+		}catch (SQLException e)
+		{
+			e.printStackTrace();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return medicos;
+	}
+	protected boolean deletarMedico(String crm)
+	{
+		boolean medicoDeletado=false;
+		try
+		{
+			Connection conexao = getConnection();
+			if (conexao != null)
+			{
+				PreparedStatement deletar = conexao.prepareStatement("DELETE FROM medico WHERE crm = '" + crm + "'");
+				medicoDeletado = deletar.executeUpdate() > 0;
+				deletar.close();
+				conexao.close();
+			}
+		}catch (SQLException e)
+		{
+			e.printStackTrace();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return medicoDeletado;
+	}
+	protected boolean atualizarMedico(Medico medico)
+	{
+		boolean medicoAtualizado=false;
+		try
+		{
+			Connection conexao = getConnection();
+			if (conexao != null)
+			{
+				PreparedStatement atualizar = conexao.prepareStatement("UPDATE medico set nome=?,crm=?,especialidade=?");
+				atualizar.setString(1,medico.getNome());
+				atualizar.setString(2,medico.getCrm());
+				atualizar.setString(3,medico.getEspecialidade());
+				medicoAtualizado = atualizar.executeUpdate() > 0;
+				atualizar.close();
+				conexao.close();
+			}
+		}catch (SQLException e)
+		{
+			e.printStackTrace();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return medicoAtualizado;
 	}
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,59 +1,116 @@
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Principal {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int opcao=0;
-		while (opcao!=3)
+		while (opcao!=6)
 		{
-			System.out.println("1- Cadastrar um mÃ©dico");
-			System.out.println("2- Consultar um mÃ©dico");
-			System.out.println("3- Sair");
-			System.out.println("Digite o nÃºmero da opÃ§Ã£o escolhida:");
-			opcao=Integer.parseInt(teclado.nextLine());
-			if (opcao==1)
+			System.out.println("1- Cadastrar um médico");
+			System.out.println("2- Consultar um médico");
+			System.out.println("3- Retornar os dados de todos os médicos cadastrados");
+			System.out.println("4- Excluir um médico");
+			System.out.println("5- Alterar os dados de um médico");
+			System.out.println("6- Sair");
+			System.out.println("Digite o número da opção escolhida");
+			try
 			{
-				Medico medico=new Medico();
-				System.out.println("Informe o nome do mÃ©dico:");
-				medico.setNome(teclado.nextLine());
-				System.out.println("Informe o CRM do mÃ©dico:");
-				medico.setCrm(teclado.nextLine());
-				System.out.println("Informe a especialidade do mÃ©dico:");
-				medico.setEspecialidade(teclado.nextLine());
-				MedicoDAO medicoDAO = new MedicoDAO();
-				Medico cadastrado = medicoDAO.selecionarMedico(medico.getCrm());
-				if (cadastrado != null)
-					System.out.println("JÃ¡ existe um mÃ©dico cadastrado com este CRM.");
-				else
+				opcao=Integer.parseInt(teclado.nextLine());
+				if (opcao==1)
 				{
-					int retorno=medicoDAO.cadastrarMedico(medico);
-					if (retorno == 1)
-						System.out.println("MÃ©dico cadastrado com sucesso!");
+					Medico medico=new Medico();
+					System.out.println("Informe o nome do médico:");
+					medico.setNome(teclado.nextLine());
+					System.out.println("Informe o CRM do médico:");
+					medico.setCrm(teclado.nextLine());
+					System.out.println("Informe a especialidade do médico:");
+					medico.setEspecialidade(teclado.nextLine());
+					MedicoDAO medicoDAO = new MedicoDAO();
+					Medico cadastrado = medicoDAO.selecionarMedico(medico.getCrm());
+					if (cadastrado != null)
+						System.out.println("Já existe um médico cadastrado com este CRM.");
 					else
-						System.out.println("Ocorreu um erro ao tentar cadastrar um MÃ©dico!");
-				}
-				System.out.println("Digite uma tecla qualquer para continuar:");
-				teclado.nextLine();
-			}else if (opcao==2)
-			{
-				System.out.println("Informe o CRM do mÃ©dico que deseja pesquisar os dados:");
-				String crm = teclado.nextLine();
-				MedicoDAO medicoDAO = new MedicoDAO();
-				Medico medico = medicoDAO.selecionarMedico(crm);
-				if (medico != null)
+					{
+						int retorno=medicoDAO.cadastrarMedico(medico);
+						if (retorno == 1)
+							System.out.println("Médico cadastrado com sucesso!");
+						else
+							System.out.println("Ocorreu um erro ao tentar cadastrar um Médico!");
+					}
+					System.out.println("Digite uma tecla qualquer para continuar:");
+					teclado.nextLine();
+				}else if (opcao==2)
 				{
-					System.out.println("InformaÃ§Ãµes do mÃ©dico:");
-					System.out.println("Nome: " + medico.getNome());
-					System.out.println("CRM: " + medico.getCrm());
-					System.out.println("Especialidade: " + medico.getEspecialidade());
-				}else
-					System.out.println("MÃ©dico nÃ£o encontrado com esse CRM!");
+					System.out.println("Informe o CRM do médico que deseja pesquisar os dados:");
+					String crm = teclado.nextLine();
+					MedicoDAO medicoDAO = new MedicoDAO();
+					Medico medico = medicoDAO.selecionarMedico(crm);
+					if (medico != null)
+					{
+						System.out.println("Informações do médico:");
+						System.out.println("Nome: " + medico.getNome());
+						System.out.println("CRM: " + medico.getCrm());
+						System.out.println("Especialidade: " + medico.getEspecialidade());
+					}else
+						System.out.println("Médico não encontrado com esse CRM!");
+					System.out.println("Digite uma tecla qualquer para continuar");
+					teclado.nextLine();
+				}else if (opcao==3)
+				{
+					MedicoDAO medicoDAO = new MedicoDAO();
+					List<Medico> medicos = medicoDAO.selecionarTodosMedicos();
+					if (!medicos.isEmpty())
+					{
+						for (Medico c:medicos)
+						{
+							System.out.println("______________________________");
+							System.out.println("Nome: " + c.getNome());
+							System.out.println("CRM: " + c.getCrm());
+							System.out.println("Especialidade: " + c.getEspecialidade());
+						}
+					}else
+						System.out.println("Nenhum médico cadastrado no momento!");
+					System.out.println("Digite uma tecla qualquer para continuar");
+					teclado.nextLine();
+				}else if (opcao==4)
+				{
+					System.out.println("Informe o CRM do medico que deseja excluir");
+					String crm = teclado.nextLine();
+					MedicoDAO medicoDAO = new MedicoDAO();
+					boolean medicoDeletado = medicoDAO.deletarMedico(crm);
+					if (medicoDeletado == true)
+						System.out.println("Medico deletado com sucesso!");
+					else
+						System.out.println("Nenhum medico encontrado com este CRM!");
+					System.out.println("Digite uma tecla qualquer para continuar");
+					teclado.nextLine();
+				}else if (opcao==5)
+				{
+					Medico medico=new Medico();
+					System.out.println("Informe o CRM do cliente que deseja alterar os dados");
+					medico.setCrm(teclado.nextLine());
+					System.out.println("Informe o nome correto do medico");
+					medico.setNome(teclado.nextLine());
+					System.out.println("Informe a especialidade correta do medico");
+					medico.setEspecialidade(teclado.nextLine());
+						MedicoDAO medicoDAO = new MedicoDAO();
+						boolean medicoAtualizado = medicoDAO.atualizarMedico(medico);
+						if (medicoAtualizado == true)
+							System.out.println("Medico atualizado com sucesso!");
+						else
+							System.out.println("Nenhum Medico encontrado com este CRM!");
+				}else if (opcao!=6)
+					System.out.println("Opção inválida");
+				else
+					teclado.close();
+			}catch (Exception erro)
+			{
+				System.out.println("Opção inválida");
 				System.out.println("Digite uma tecla qualquer para continuar");
 				teclado.nextLine();
-			}else if (opcao!=3)
-				System.out.println("OpÃ§Ã£o invÃ¡lida");
-			else
-				teclado.close();
+			}
 		}
 	}
 }
